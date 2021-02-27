f50d842e8d1b53636df6db08033917dd2fc119f0
shairaalam19 <71992258+shairaalam19@users.noreply.github.com>
2021-02-26 17:03:47 -0700

    Update GitDemo.java
    Edited line 2 to have name
    addNumber() to set previousNumber += num; 
    Added private double variable called "reverseNum"
    reverseNumber() added line with reverseNum = newNumber;

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Shaira Alam
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,6 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
+        previousNumber += num;
         return 0;
     }
     
@@ -46,6 +48,7 @@ public class GitDemo {
             newNumber = newNumber * i;
         }
         
+        reverseNum = newNumber;
         return newNumber;
     }
     
