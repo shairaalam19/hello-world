a46bb244caa6a3243fcdc59fa93dbb23ad0625e1
shairaalam19 <71992258+shairaalam19@users.noreply.github.com>
2021-02-26 17:12:21 -0700

    Update GitDemo.java
    Switched the contents within the {} for the findFactorial(num) and reverseNumber()
    Switched the variables in findFactorial() from newNumber to factorialNumber
    swithced variables in reverseNumber() from factorialNumber to newNumber

--- GitDemo.java
+++ GitDemo.java
@@ -23,12 +23,10 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 0;
+        double factorialNumber = 1;
         
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+        for(int i = 1; i <= num; ++i){
+            factorialNumber = factorialNumber * i;
         }
         
         return factorialNumber;
@@ -39,11 +37,11 @@ public class GitDemo {
      * i.e. if num = 1234
      *  the function will return 4321
      *****/
-    public double reverseNumber(int num){
-        double newNumber = 1;
-        
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
+    public double reverseNumber(int num){     
+        while( num != 0 ) {
+            double temp = num % 10;
+            newNumber = newNumber * 10 + temp;
+            num /= 10;
         }
         
         return newNumber;
